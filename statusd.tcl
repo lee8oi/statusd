d70d55c4349d1a9cccb25a7e2ff79204dcdc3fc2
Lee Forest <lee8oi@gmail.com>
2011-09-27 18:57:38 -0400

    bugfix

--- statusd.tcl
+++ statusd.tcl
@@ -175,7 +175,7 @@ namespace eval statusd {
       puts $fs "variable ::statusd::statustext"
       puts $fs "variable ::statusd::nickhost"
       # create 'array set' lines using array data.
-      foreach arr {status nickcase lastchan statustime statustext nickhost} {
+      foreach arr {status nickcase lastchan statustime statustext} {
          puts $fs "array set $arr [list [array get $arr]]"
       }
       close $fs;

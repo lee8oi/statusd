916c1f3cae638ce528a104b9104de9b0da500567
Lee Forest <lee8oi@gmail.com>
2011-09-27 20:37:15 -0400

    unstable testing

--- statusd.tcl
+++ statusd.tcl
@@ -176,7 +176,8 @@ namespace eval statusd {
       puts $fs "variable ::statusd::nickhost"
       # create 'array set' lines using array data.
       foreach arr {status nickcase lastchan statustime statustext nickhost} {
-         puts $fs "array set $arr [list [array get $arr] ] "
+         puts $fs "[list [array get $arr]]"
+         #puts $fs "array set $arr [list [array get $arr]]"
       }
       close $fs;
       if {[set ::statusd::logbackups]} {
